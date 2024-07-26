3786b923c06d51214997bdacaede5bfa88f6674a
Keith Alpichi <keithalpichi@users.noreply.github.com>
2024-07-26 16:47:34 -0700

    Fix volume & mute functionality

--- src/volume.ts
+++ src/volume.ts
@@ -3,29 +3,21 @@ export default class Volume {
 	static MIN = 0;
 	static MAX = 1;
 	gainNode: GainNode
+	isMuted: boolean
 	private lastKnownVolume: number 
 	constructor({ context }: { context: AudioContext }) {
-		this.gainNode = new GainNode(context);
+		// set the initial gain to 100%
+		this.gainNode = new GainNode(context, { gain: 1 });
 		this.lastKnownVolume = this.gainNode.gain.value;
+		this.isMuted = false
 	}
 
 	get currentVolume(): number {
 		return this.gainNode.gain.value;
 	}
 
-	get isMuted(): boolean {
-		return this.currentVolume === Volume.MIN;
-	}
-
 	get atMaxVolume(): boolean {
 		return this.currentVolume === Volume.MAX;
-	}
-
-	private _set(percent: number): void {
-		if (percent < Volume.MIN || percent > Volume.MAX) {
-			throw Volume.InvalidVolumePercentage
-		}
-		this.gainNode.gain.value = percent;
 	}
 
 	toggleMute(): void {
@@ -37,28 +29,44 @@ export default class Volume {
 	}
 
 	mute(): void {
-		if (this.currentVolume !== Volume.MIN) {
-			this.lastKnownVolume = this.currentVolume;
-			this._set(Volume.MIN);
+		if (this.isMuted) {
+			// we're muted, return
+			return 
 		}
+		// before changing the volume,
+		// take note of the current volume
+		// so we can set it to the last known volume
+		// when we unmute
+		this.lastKnownVolume = this.gainNode.gain.value;
+		// mute it now
+		this.gainNode.gain.value = Volume.MIN;
+		this.isMuted = true
+
 	}
 
 	unmute(): void {
-		if (this.currentVolume === Volume.MIN) {
-			this._set(this.lastKnownVolume);
+		if (!this.isMuted) {
+			// we have volume, return
+			return
 		}
+		this.isMuted = false
+		// update the volume to the last known volume
+		this.gainNode.gain.value = this.lastKnownVolume;
 	}
 
 	set(percent: number): void {
-		if (percent < Volume.MIN || percent > Volume.MAX) {
-			throw Volume.InvalidVolumePercentage
+		if (!this.isMuted) {
+			// we're not muted, the last known value should
+			// match the new value
+			this.lastKnownVolume = percent;
+			this.gainNode.gain.value = percent;
+		} else {
+			// we're muted, just keep track of the new volume;
+			this.lastKnownVolume = percent;
 		}
-		this.lastKnownVolume = this.currentVolume;
-		this._set(percent);
 	}
 
 	max(): void {
-		this.lastKnownVolume = this.currentVolume;
-		this._set(Volume.MAX);
+		this.set(Volume.MAX);
 	}
 }
